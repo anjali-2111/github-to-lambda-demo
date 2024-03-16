78e66b1828f5d7b9074e213ff405e74176634c7b
anjali-2111 <anjalisikhwal717@gmail.com>
2024-03-16 17:01:12 +0530

    changed

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ import requests
 import pandas as pd
 
 def lambda_handler(event, context):
-    
     print("Event Data -> ", event)
     response = requests.get("https://www.google.com/")
     print(response.text)
